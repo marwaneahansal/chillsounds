f3db2e37f36e80a1bbc52cb4eaf327f5edfa65c6
Marwane Ahansal <ahansalmarwane@gmail.com>
2023-04-18 23:37:07 +0000

    added chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
